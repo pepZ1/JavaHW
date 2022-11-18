d1dd9431f21373393d6f63aed79c079411a55a3f
aketo <aketo13@gmail.com>
2022-11-18 11:10:18 +0400

    Task1

---
+++ HW1/task1.java
@@ -0,0 +1,16 @@
+// Реализовать таблицу истинности т.е у нас есть p = false и q = true.
+// Чему будет равно p & q == ? , ? - вынести в таблицу.
+
+package HW1;
+
+public class task1 {
+    public static void main(String[] args) {
+        boolean[] bool = {false, false, true, false, false, true, true, true};
+        System.out.println("  p\t\t" + "  q\t\t" + "p & q\tp | q\tp ^ q \t !p");
+        for (int i = 0; i < bool.length; i++) {
+            boolean p = bool[i++];
+            boolean q = bool[i];
+            System.out.println(p + "\t" + q + "\t" + (p & q) + "\t" + (p | q) + "\t" + (p ^ q) + "\t" + !p);
+        }
+    }
+}
