d808411d81228cb79fc0c970b7ace6ef786ba4e5
aketo <aketo13@gmail.com>
2022-11-27 13:32:39 +0400

    Task1+

--- HW3/task1.java
+++ HW3/task1.java
@@ -9,7 +9,7 @@ public class task1 {
     public static void main(String[] args) {
 
         List<Integer> Evenray = new ArrayList<Integer>();
-        Evenray.addAll(Arrays.asList(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6));
+        Evenray.addAll(Arrays.asList(1, 1, 2, 2, 3, 3, 4, 5, 6));
         System.out.println(Evenray);
 
         for (int i = 0; i < Evenray.size(); i++) {
